716dead9357e2c56c560ae137cad8384d6ca36c9
jo541 <touba.john@gmail.com>
2015-01-24 02:42:20 +0100

    Creation des barres de progression

---
+++ Game/Assets/Scripts/ProgresBar.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ProgresBar : MonoBehaviour
+{
+
+
+    #region Attributes
+    public GameObject statBar;
+    #endregion
+
+    #region Unity Methods
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #region Other Methods
+    public void SetStats(float value) 
+    {
+
+        statBar.GetComponent<Scrollbar>().size +=value;
+    }
+    #endregion
+
+
+}
