564cfa80dfde9a12358c67beee567ad104cd2523
jo541 <touba.john@gmail.com>
2015-01-25 14:22:10 +0100

    Sons

---
+++ Game/Assets/Scripts/Chargement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chargement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void OnMouseDown()
+    {
+        Debug.Log("test");
+        Application.LoadLevel(1);
+    }
+}
